a50c9af4f9adf414cf787aad9f616fdd57bbd1bc
alexandre <alexandre@pandascore.co>
2022-01-28 22:23:43 +0100

    fix some imports again

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import Annotation from 'Annotation.svelte';
-import Chart from 'Chart.svelte';
-import Grid from 'Grid.svelte';
-import Legend from 'Legend.svelte';
-import PlotOptions from 'PlotOptions.svelte';
-import Serie from 'Serie.svelte';
-import Title from 'Title.svelte';
-import Tooltip from 'Tooltip.svelte';
-import XAxis from 'XAxis.svelte';
-import YAxis from 'YAxis.svelte';
+import Annotation from './Annotation.svelte';
+import Chart from './Chart.svelte';
+import Grid from './Grid.svelte';
+import Legend from './Legend.svelte';
+import PlotOptions from './PlotOptions.svelte';
+import Serie from './Serie.svelte';
+import Title from './Title.svelte';
+import Tooltip from './Tooltip.svelte';
+import XAxis from './XAxis.svelte';
+import YAxis from './YAxis.svelte';
 
 export {Annotation, Chart, Grid, Legend, PlotOptions, Serie, Title, Tooltip, XAxis, YAxis}
