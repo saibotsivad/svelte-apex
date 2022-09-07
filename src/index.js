2e15dfc79513f8dc535989b40863fc1fe4ad3e0b
Tobias Davis <accounts@davistobias.com>
2022-09-07 15:23:31 -0500

    fix: "serie" to "series"

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import Chart from './Chart.svelte';
 import Grid from './Grid.svelte';
 import Legend from './Legend.svelte';
 import PlotOptions from './PlotOptions.svelte';
-import Serie from './Serie.svelte';
+import Series from './Series.svelte';
 import Title from './Title.svelte';
 import Tooltip from './Tooltip.svelte';
 import XAxis from './XAxis.svelte';
 import YAxis from './YAxis.svelte';
 
-export {Annotation, Chart, Grid, Legend, PlotOptions, Serie, Title, Tooltip, XAxis, YAxis}
+export {Annotation, Chart, Grid, Legend, PlotOptions, Series, Title, Tooltip, XAxis, YAxis}
