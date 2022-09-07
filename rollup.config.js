5e155dbba26c4b4157c4a5ba135de19316763a11
Tobias Davis <accounts@davistobias.com>
2022-09-07 15:22:00 -0500

    fix: "serie" to "series"

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,18 @@ import resolve from 'rollup-plugin-node-resolve';
 const pkg = require('./package.json');
 
 export default {
-	input: ['src/Annotation.svelte','src/Chart.svelte','src/Grid.svelte', 'src/Legend.svelte', 'src/PlotOptions.svelte', 'src/Serie.svelte',
-	'src/Title.svelte', 'src/Tooltip.svelte', 'src/Xaxis.svelte', 'src/YAxis.svelte'],
+	input: [
+		'src/Annotation.svelte',
+		'src/Chart.svelte',
+		'src/Grid.svelte',
+		'src/Legend.svelte',
+		'src/PlotOptions.svelte',
+		'src/Series.svelte',
+		'src/Title.svelte',
+		'src/Tooltip.svelte',
+		'src/Xaxis.svelte',
+		'src/YAxis.svelte',
+	],
 	output: [
 		{ file: pkg.module, 'format': 'en' },
 		{ file: pkg.main, 'format': 'umd', name: 'SvelteApex' }
